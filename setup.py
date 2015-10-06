df58a4172cbb839b2b4edbba20686d1e5d1317a4
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-06 23:13:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     packages=(
         'django_call_after_commit',
     ),
+    url='https://chris-lamb.co.uk/projects/django-call-after-commit',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Django utility to easily queue methods to be called after current transaction has been committed",
     install_requires=['django>=1.0'],
-    description="",
 )
